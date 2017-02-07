6565ca394dff282d0d07789569b5aaf4ca7db87b
parrotdies <brandon.fengys@gmail.com>
2017-02-07 17:28:47 -0500

    Add files via upload

---
+++ Lab 1/LifeCycle.h
@@ -0,0 +1,27 @@
+//Brandon Feng yf2km  1/24/2017 LifeCycle.h
+#ifndef LIFECYCLE_H
+#define INTCELL_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+// ---------------------------------------------------  class definition
+class MyObject {
+public:
+    static int numObjs;
+    MyObject(const char *n = "--default--");      // default constructor
+    MyObject(const MyObject& rhs);                // copy constructor
+    ~MyObject();                                  // destructor
+    string getName() const {
+        return name;
+    }
+    void setName(const string newName) {
+        name = newName;
+    }
+    friend ostream& operator<<(ostream& output, const MyObject& obj);
+private:
+    string name;
+    int id;
+};
+
+#endif
